d0c9936ed2f9fb688b8724051d0207eacf38722c
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-05-15 19:02:41 +0600

    explorin json stringify and parse

---
+++ js/json.js
@@ -0,0 +1,31 @@
+/*
+const object={
+    name:'Mahabub',
+    id:1295223,
+    address:{
+        division:'dhaka',
+        district:'Nilphamari',
+        currentAddress:'Dhanmodi'
+    }
+
+}
+const stringified=JSON.stringify(object);
+console.log(stringified);
+*/
+
+const shop={
+    owner:'alil',
+    dddress:{
+        streed:'dhanmondi',
+        city:'dhaka',
+        country:'Bangladesh'
+    },
+    product_details:{
+        brand:'lenove',
+        price:50000
+
+    }
+}
+
+const shopString=JSON.stringify(shop);
+console.log(shopString)
